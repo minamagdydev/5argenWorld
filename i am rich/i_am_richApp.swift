e7b48e39ed0b632cd2e0b52d5e1b622fb572b423
mina <mina.magdy@parent.cloud>
2023-11-13 15:16:37 +0200

    Initial Commit

---
+++ i am rich/i_am_richApp.swift
@@ -0,0 +1,17 @@
+//
+//  i_am_richApp.swift
+//  i am rich
+//
+//  Created by mina on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct i_am_richApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
